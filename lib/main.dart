8f43ba1afb99231bce07079825f9290de7c5a02e
Daniel Zaczek <50755378+dezet98@users.noreply.github.com>
2021-09-23 16:31:26 +0200

    add intl

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:codetomobile/shared/logger/simple_bloc_observer.dart';
 import 'package:codetomobile/ui/screens/astronomical_object_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   Bloc.observer = SimpleBlocObserver();
@@ -12,7 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Genetic algorithms',
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context)!.title,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/ui/screens/astronomical_object_list.dart
+++ lib/ui/screens/astronomical_object_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AstronomicalObjectList extends StatelessWidget {
   const AstronomicalObjectList({Key? key}) : super(key: key);
@@ -8,7 +9,7 @@ class AstronomicalObjectList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello"),
+        child: Text(AppLocalizations.of(context)!.title),
       ),
     );
   }
